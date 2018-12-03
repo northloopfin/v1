90459af468b1197b63e1478dd25de1318b67903f
Daffodil <sunita.kumari@daffodilsw.com>
2018-12-03 13:32:36 +0530

    Initial Commit : Blank Project

---
+++ NorthLoopFin/NorthLoopFin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NorthLoopFin
+//
+//  Created by Daffodil on 03/12/18.
+//  Copyright © 2018 NorthLoop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
