08078868f86c0543b5c278af4a19e1fd08761357
sunita kumari <sunita.kumari@daffodilsw.com>
2018-12-12 11:53:30 +0530

    -Home BG Added -Fonts Added -Navigation controller added in AppDelegate -Home Page UI partially Done

---
+++ NorthLoopFin/NorthLoopFin/Application Views/Home/View/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  NorthLoopFin
+//
+//  Created by Daffolapmac-19 on 12/12/18.
+//  Copyright © 2018 NorthLoop. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
