0cfefa3d4069f2392b43637432bed92caa7d6790
sunita kumari <sunita.kumari@daffodilsw.com>
2018-12-12 14:56:44 +0530

    -Custom Table cell added -Custom table header added

---
+++ NorthLoopFin/NorthLoopFin/Application Views/Home/View/HomeTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableCell.swift
+//  NorthLoopFin
+//
+//  Created by Daffolapmac-19 on 12/12/18.
+//  Copyright © 2018 NorthLoop. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ NorthLoopFin/NorthLoopFin/Application Views/Home/View/HomeTableSectionCell.swift
@@ -0,0 +1,21 @@
+//
+//  HomeTableSectionCell.swift
+//  NorthLoopFin
+//
+//  Created by Daffolapmac-19 on 12/12/18.
+//  Copyright © 2018 NorthLoop. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableSectionCell: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
