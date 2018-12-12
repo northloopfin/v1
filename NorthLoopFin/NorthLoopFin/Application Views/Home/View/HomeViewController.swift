be37b9c3912d1029b725cdb2ad6b6f198c214d61
sunita kumari <sunita.kumari@daffodilsw.com>
2018-12-12 15:30:18 +0530

    -Home Page UI Complete

--- NorthLoopFin/NorthLoopFin/Application Views/Home/View/HomeViewController.swift
+++ NorthLoopFin/NorthLoopFin/Application Views/Home/View/HomeViewController.swift
@@ -9,22 +9,51 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    @IBOutlet weak var ledgersTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureTable()
+        self.ledgersTableView.reloadData()
 
         // Do any additional setup after loading the view.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension HomeViewController:UITableViewDelegate,UITableViewDataSource{
+    //Configure table view delgates and data source
+    func configureTable(){
+        self.ledgersTableView.rowHeight = 44;
+        self.ledgersTableView.delegate=self
+        self.ledgersTableView.dataSource=self
+        self.ledgersTableView.registerTableViewCell(tableViewCell: HomeTableCell.self)
+        self.ledgersTableView.registerTableViewHeaderFooterView(tableViewHeaderFooter: HomeTableSectionCell.self)
     }
-    */
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: HomeTableCell = tableView.dequeueReusableCell(withIdentifier: "HomeTableCell") as! HomeTableCell
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        return 70.0
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView?{
+        
+        let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "HomeTableSectionCell") as! HomeTableSectionCell
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        
+        return 70
+    }
 }
