33a6b5635b0a5eb6006b4b3563910c51c1d5d8ad
sunita kumari <sunita.kumari@daffodilsw.com>
2019-05-15 12:17:34 +0530

    added this file

---
+++ NorthLoopFin/NorthLoopFin/Application Views/TransactionDetail/View/TransactionDetailDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  TransactionDetailDelgates.swift
+//  NorthLoopFin
+//
+//  Created by Daffolapmac-19 on 14/05/19.
+//  Copyright © 2019 NorthLoop. All rights reserved.
+//
+
+import Foundation
+
+//class TransactionDetailDelegate: BaseViewProtocol {
+//    
+//    
+//}
